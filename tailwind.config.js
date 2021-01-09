54f8eb9fb2ee5e5a28db84228f34c105525fd309
kureharyosuke <ohhayo.com@gmail.com>
2021-01-10 08:08:05 +0900

    #4. touch postcss.config.js (v 2.0.2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
+// tailwind.config.js
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
   },
-  variants: {
-    extend: {}
-  },
+  variants: {},
   plugins: []
 };
